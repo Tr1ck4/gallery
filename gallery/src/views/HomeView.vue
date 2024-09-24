5238828b0d8c1b4aa8844e5324424b7f191f17b1
Tr1ck4 <91774874+Tr1ck4@users.noreply.github.com>
2024-09-24 10:31:15 +0700

    mapload

--- gallery/src/views/HomeView.vue
+++ gallery/src/views/HomeView.vue
@@ -19,13 +19,16 @@
 
 <script>
 import * as THREE from 'three';
+import Stats from 'three/addons/libs/stats.module.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { MapControls } from 'three/addons/controls/MapControls.js';
 import wallTexture from '../assets/khazik.png';
 import texture_1 from '../assets/1.jpeg';
 import texture_2 from '../assets/2.jpg';
 import texture_3 from '../assets/3.jpg';
+import wall_1 from '../assets/wall1.jpg';
+import wall_2 from '../assets/wall2.jpg';
+import wall_3 from '../assets/wall3.jpg';
 
 export default {
   data() {
@@ -50,158 +53,165 @@ export default {
       isTooltip: false,
       torus: null,
       keys: {},
+      stats: new Stats(),
     };
   },
   mounted() {
     this.initThreeScene();
     this.loadModel();
-    window.addEventListener('resize', this.onWindowResize);
-    window.addEventListener('keydown', this.onKeyDown);
+    //window.addEventListener('resize', this.onWindowResize);
+    // window.addEventListener('keydown', this.onKeyDown);
 
     document.addEventListener('keydown', this.keyDownHandler);
     document.addEventListener('keyup', this.keyUpHandler);
   },
   methods: {
     initThreeScene() {
+      
       const scene = new THREE.Scene();
-      scene.background = new THREE.Color(0xffffff);
-
-      // Camera Setup
-      this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100);
-      this.camera.position.set(10, 5, 10);
-      this.camera.lookAt(0, 4, 0);
+      scene.background = new THREE.Color( 0xcccccc );
+      scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
 
       // Renderer Setup
-      this.renderer = new THREE.WebGLRenderer({ antialias: true });
+      this.renderer = new THREE.WebGLRenderer({ antialias: false });
       this.renderer.setSize(window.innerWidth, window.innerHeight);
-      this.renderer.shadowMap.enabled = true;
+      this.renderer.setPixelRatio( window.devicePixelRatio);
+      this.renderer.setAnimationLoop( this.animate );
       this.$refs.sceneContainer.appendChild(this.renderer.domElement);
 
+      this.stats = new Stats();
+      this.$refs.sceneContainer.appendChild( this.stats.dom );
+      
+      // Camera Setup
+      // this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 100);
+      // this.camera.position.set(10, 5, 10);
+      // this.camera.lookAt(0, 4, 0);
+
+      this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 100 );
+      this.camera.position.set( 0, 20, - 40 );
+
       // Orbit Controls
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      this.controls = new MapControls(this.camera, this.renderer.domElement);
+      this.controls.enableDamping = true;
+      this.controls.dampingFactor = 0.05;
+      this.controls.screenSpacePanning = false;
+      this.controls.minDistance = 50;
+      this.controls.maxDistance = 500;
+
+      this.controls.maxPolarAngle = Math.PI/2;
+
+      // Importing map texture
+      const texture_1 = new THREE.TextureLoader().load(wall_1);
+      texture_1.colorSpace = THREE.SRGBColorSpace;
+      
+      const texture_2 = new THREE.TextureLoader().load(wall_2);
+      texture_2.colorSpace = THREE.SRGBColorSpace;
+      
+      const texture_3 = new THREE.TextureLoader().load(wall_3);
+      texture_3.colorSpace = THREE.SRGBColorSpace;
+
+      const house_1 = new THREE.MeshPhongMaterial({ map: texture_1, flatShading: true });
+      const house_2 = new THREE.MeshPhongMaterial({ map: texture_2, flatShading: true });
+      const house_3 = new THREE.MeshPhongMaterial({ map: texture_3, flatShading: true });
+      const house_texture = [house_1, house_2, house_3];
+
+      // Init house value
+      const block = new THREE.BoxGeometry();
+			block.translate( 0, 0.5, 0 );
+
+      // Generating map
+      for (let i = 0 ; i < 500 ; i++) {
+        const mesh = new THREE.Mesh(block, house_texture[Math.random( house_texture.length)]);
+        mesh.position.x = Math.random() * 160 - 80;
+        mesh.position.y = 0;
+        mesh.position.z = Math.random() * 160 - 80;
+        mesh.scale.x = 2;
+        mesh.scale.y = Math.random() * 8 + 1;
+        mesh.scale.z = 2;
+        mesh.updateMatrix();
+        mesh.matrixAutoUpdate = false;
+        scene.add(mesh);
+
+      }
 
       // Lighting
-      const spotLight = new THREE.SpotLight(0xff0000, 10, 10);
-      spotLight.position.set(1, 5, 1);
-      spotLight.castShadow = true;
-      scene.add(spotLight);
+      const dirLight1 = new THREE.DirectionalLight( 0xffffff, 3 );
+      dirLight1.position.set( 1, 1, 1 );
+      scene.add( dirLight1 );
 
-      const texture = new THREE.TextureLoader().load(wallTexture);
-      texture.wrapS = THREE.RepeatWrapping;
-      texture.wrapT = THREE.RepeatWrapping;
+      const dirLight2 = new THREE.DirectionalLight( 0x002288, 3 );
+      dirLight2.position.set( - 1, - 1, - 1 );
+      scene.add( dirLight2 );
 
-      const wallMaterial = new THREE.MeshBasicMaterial({ map: texture });
+      const ambientLight = new THREE.AmbientLight( 0x555555 );
+      scene.add( ambientLight );
 
-      const color = 0xFFFFFF;
-      const intensity = 100;
-      const light = new THREE.SpotLight( color, intensity );
-      light.castShadow = true;
-      light.position.set( 5, 20, 5 );
-      light.target.position.set( - 4, 0, - 4 );
+      // const area = new THREE.TorusGeometry(1,0.1,16,100);
+      // const material = new THREE.MeshBasicMaterial({color: 0xffff00});
+      // const torus = new THREE.Mesh(area, material);
+      // torus.position.set(1,0,3);
+      // torus.rotation.x = 1.6;
 
-      const SpotLightHelper = new THREE.SpotLightHelper(light);
-      scene.add( light );
-      scene.add( light.target );
-      scene.add( SpotLightHelper);
-    
-      const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100 ), new THREE.MeshPhongMaterial( { color: 0xcbcbcb, depthWrite: false } ) );
-      mesh.rotation.x = - Math.PI / 2;
-      mesh.receiveShadow = true;
-      scene.add( mesh );
-
-      const wall_1 = new THREE.BoxGeometry(10, 10, 1);
-      const wall_1_prefab = new THREE.Mesh(wall_1, new THREE.MeshBasicMaterial({ color: '#ffff00' }));
-      wall_1_prefab.position.set(0, 4.5, -5.5);
-      wall_1_prefab.castShadow = true;
-      scene.add(wall_1_prefab);
-
-      const wall_2 = new THREE.BoxGeometry(1, 10, 10);
-      const wall_2_prefab = new THREE.Mesh(wall_2, new THREE.MeshBasicMaterial({ color: '#ffff00' }));
-      wall_2_prefab.position.set(-5.5, 4.5, 0);
-      wall_2_prefab.castShadow = true;
-      scene.add(wall_2_prefab);
-
-      const frame_1 = new THREE.BoxGeometry(1, 1, 0.1);
-      const frame_1_prefab = new THREE.Mesh(frame_1, wallMaterial);
-      frame_1_prefab.position.set(2, 4, -5);
-      scene.add(frame_1_prefab);
-
-      const frame_2_prefab = new THREE.Mesh(frame_1, wallMaterial);
-      frame_2_prefab.position.set(0, 4, -5);
-      scene.add(frame_2_prefab);
-
-      const frame_3_prefab = new THREE.Mesh(frame_1, wallMaterial);
-      frame_3_prefab.position.set(-2, 4, -5);
-      scene.add(frame_3_prefab);
-
-      const area = new THREE.TorusGeometry(1,0.1,16,100);
-      const material = new THREE.MeshBasicMaterial({color: 0xffff00});
-      const torus = new THREE.Mesh(area, material);
-      torus.position.set(1,0,3);
-      torus.rotation.x = 1.6;
-
-      scene.add(torus);
-      this.torus = torus;
+      // scene.add(torus);
+      // this.torus = torus;
 
 
-      const video = document.createElement('video');
-      video.src = 'assets/random.mp4'; 
-      video.crossOrigin = 'anonymous'; 
-      video.loop = true;
-      video.muted = true;
-      video.playsInline = true;
-      video.autoplay = true;
-      video.play();
+      // const video = document.createElement('video');
+      // video.src = 'assets/random.mp4'; 
+      // video.crossOrigin = 'anonymous'; 
+      // video.loop = true;
+      // video.muted = true;
+      // video.playsInline = true;
+      // video.autoplay = true;
+      // video.play();
 
-      const videoTexture = new THREE.VideoTexture(video);
+      // const videoTexture = new THREE.VideoTexture(video);
 
-      const videoMaterial = new THREE.MeshBasicMaterial({ map: videoTexture });
+      // const videoMaterial = new THREE.MeshBasicMaterial({ map: videoTexture });
 
-      const sideMaterial1 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-      const sideMaterial2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); 
-      const sideMaterial3 = new THREE.MeshBasicMaterial({ color: 0x0000ff }); 
-      const sideMaterial4 = new THREE.MeshBasicMaterial({ color: 0xffff00 }); 
-      const sideMaterial5 = new THREE.MeshBasicMaterial({ color: 0x00ffff }); 
+      // const sideMaterial1 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+      // const sideMaterial2 = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); 
+      // const sideMaterial3 = new THREE.MeshBasicMaterial({ color: 0x0000ff }); 
+      // const sideMaterial4 = new THREE.MeshBasicMaterial({ color: 0xffff00 }); 
+      // const sideMaterial5 = new THREE.MeshBasicMaterial({ color: 0x00ffff }); 
 
-      const materials = [
-        videoMaterial,  
-        sideMaterial2,  
-        sideMaterial1,  
-        sideMaterial3,  
-        sideMaterial4,  
-        sideMaterial5   
-      ];
+      // const materials = [
+      //   videoMaterial,  
+      //   sideMaterial2,  
+      //   sideMaterial1,  
+      //   sideMaterial3,  
+      //   sideMaterial4,  
+      //   sideMaterial5   
+      // ];
 
-      const geometry = new THREE.BoxGeometry(0.5, 6, 9);
-      const cube = new THREE.Mesh(geometry, materials);
-      cube.position.set(-5,5,0);
-      scene.add(cube);
+      // const geometry = new THREE.BoxGeometry(0.5, 6, 9);
+      // const cube = new THREE.Mesh(geometry, materials);
+      // cube.position.set(-5,5,0);
+      // scene.add(cube);
 
-      const onClick = (event) => {
-        this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-        this.raycaster.setFromCamera(this.mouse, this.camera);
-        const intersects = this.raycaster.intersectObject(cube);
-        if (intersects.length > 0) {
-          this.isFadingOut = true;
+      // const onClick = (event) => {
+      //   this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      //   this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+      //   this.raycaster.setFromCamera(this.mouse, this.camera);
+      //   const intersects = this.raycaster.intersectObject(cube);
+      //   if (intersects.length > 0) {
+      //     this.isFadingOut = true;
 
-          setTimeout(() => {
-            document.getElementById('fullscreen-video').style.display = 'block';
-            this.isFadingIn = true;
-            document.getElementById('fullscreen-video').play();
-            this.isButtonVisible = true;
-          }, 1000);
-        }
-      }
-      window.addEventListener('click', onClick);
+      //     setTimeout(() => {
+      //       document.getElementById('fullscreen-video').style.display = 'block';
+      //       this.isFadingIn = true;
+      //       document.getElementById('fullscreen-video').play();
+      //       this.isButtonVisible = true;
+      //     }, 1000);
+      //   }
+      // }
+      // window.addEventListener('click', onClick);
 
       // Attach the scene to this
       this.scene = scene;
 
       // Start rendering
       this.animate();
-
-
     },
     loadModel() {
       const loader = new GLTFLoader();
@@ -233,7 +243,9 @@ export default {
 
     },
     animate() {
+      
       const clock = new THREE.Clock();
+      this.controls.update();
       const renderLoop = () => {
         const delta = clock.getDelta();
         if (this.mixer) this.mixer.update(delta);
@@ -265,10 +277,11 @@ export default {
           }
         }
 
-        const bounder = new THREE.Box3().setFromObject(this.torus);
-        if( this.model != null) this.checkCollider(this.torus, bounder, this.model);
+        // const bounder = new THREE.Box3().setFromObject(this.torus);
+        // if( this.model != null) this.checkCollider(this.torus, bounder, this.model);
         // Render the scene
         this.renderer.render(this.scene, this.camera);
+        this.stats.update();
         requestAnimationFrame(renderLoop);
       };
       renderLoop();
